17b487de4862dc5a54f9bbf720de1a5feaafc80b
bhautik-senjaliya <bhautik.senjaliya@digicollect.com>
2016-09-07 19:29:27 +0530

    Changed database

--- Bagpiper/settings.py
+++ Bagpiper/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Bagpiper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'bagpiper',
+        'USER'      :'postgres',
+        'PASSWORD'  :'Sp4t10v1z',
+        'HOST'      :'127.0.0.1',
+        'PORT'      :'5432',
     }
 }
 
